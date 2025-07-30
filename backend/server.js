53ca98f759e5955cc74fe4e188695e48c8163b7d
Andy-K-Taua <>
2025-07-30 15:24:34 +1000

    changed port

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 // backend/server.js
 
-import express from 'express';
-import authRoutes from './routes/authRoutes.js';
+import app from './app.js';
 
-const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
